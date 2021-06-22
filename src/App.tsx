ad91888399df46642073b27b9a6387aa657b1312
Victor Talge <vt.talge@gmail.com>
2021-06-21 23:02:08 -0300

    21/06 Completo

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import { AuthContextProvider } from "./context/AuthContext";
+
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
