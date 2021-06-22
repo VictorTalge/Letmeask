5f9baef89d5099b3519b661aa58fc6885963482d
Victor Talge <vt.talge@gmail.com>
2021-06-21 21:41:07 -0300

    Home e New Room completa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
